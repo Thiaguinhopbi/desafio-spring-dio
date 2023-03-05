dadb323cdb42391c497468b195db3f70cb27f580
Thiago <tdsrogerio@gmail.com>
2023-03-05 20:59:49 -0300

    Padrões de projetos

---
+++ src/main/java/one/digitalinnovation/gof/model/ClienteRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository <Cliente, Long> {
+
+}
+ 
